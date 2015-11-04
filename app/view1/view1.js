30854ea31ea134a35ab8ec2d901b771ab47a18f5
Yashaswini R <yashaswini90@gmail.com>
2015-11-04 11:37:42 +0530

    First Commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,12 +3,21 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
+  $routeProvider.when('/users', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope, $http) {
+    $http.get('https://api.github.com/users').success(function (response) {
+        $scope.users = response;
+    });
 
-}]);
+    $scope.getDetails = function(login) {
+      $http.get('https://api.github.com/users/'+login).success(function (response) {
+          $scope.selectedUser = response;
+          $scope.selectedUser.avatar = "https://avatars0.githubusercontent.com/u/"+response.id+"?v=3";
+      });
+    }
+});
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,12 +3,17 @@
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+  $routeProvider.when('/user/:userId', {
+    templateUrl: function(params) {
+      return 'view2/view2.html?id=' + params.userId;
+    },
     controller: 'View2Ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
-}]);
+.controller('View2Ctrl', function($scope, $routeParams, $http) {
+  $http.get('https://api.github.com/users/'+$routeParams.userId).success(function (response) {
+      $scope.selectedUser = response;
+      $scope.selectedUser.avatar = "https://avatars0.githubusercontent.com/u/"+response.id+"?v=3";
+  });
+});
